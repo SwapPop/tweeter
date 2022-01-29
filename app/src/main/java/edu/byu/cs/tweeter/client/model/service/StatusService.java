e85ade6c92a84be18f5033f40d1870ef4a2df341
Jacob Taylor <jacob.taylor0299@gmail.com>
2022-01-28 17:29:52 -0700

    Story refactor done

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/StatusService.java
@@ -12,6 +12,7 @@ import java.util.concurrent.Executors;
 
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetFeedTask;
+import edu.byu.cs.tweeter.client.model.service.backgroundTask.GetStoryTask;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
@@ -24,11 +25,25 @@ public class StatusService {
         void handleException(Exception exception);
     }
 
+    public interface GetStoryObserver {
+        void handleSuccess(List<Status> feed, boolean hasMorePages);
+        void handleFailure(String message);
+        void handleException(Exception exception);
+    }
+
+
     public void getFeed(AuthToken currUserAuthToken, User user, int pageSize, Status lastStatus, StatusService.GetFeedObserver getFeedObserver) {
         GetFeedTask getFeedTask = new GetFeedTask(currUserAuthToken,
                 user, pageSize, lastStatus, new GetFeedHandler(getFeedObserver));
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(getFeedTask);
+    }
+
+    public void getStory(AuthToken currUserAuthToken, User user, int pageSize, Status lastStatus, StatusService.GetStoryObserver getStoryObserver) {
+        GetStoryTask getStoryTask = new GetStoryTask(currUserAuthToken,
+                user, pageSize, lastStatus, new GetStoryHandler(getStoryObserver));
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(getStoryTask);
     }
 
 
@@ -40,6 +55,36 @@ public class StatusService {
         private StatusService.GetFeedObserver observer;
 
         public GetFeedHandler(StatusService.GetFeedObserver observer) {
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+
+            boolean success = msg.getData().getBoolean(GetFeedTask.SUCCESS_KEY);
+            if (success) {
+                List<Status> statuses = (List<Status>) msg.getData().getSerializable(GetFeedTask.STATUSES_KEY);
+                boolean hasMorePages = msg.getData().getBoolean(GetFeedTask.MORE_PAGES_KEY);
+                observer.handleSuccess(statuses, hasMorePages);
+
+            } else if (msg.getData().containsKey(GetFeedTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(GetFeedTask.MESSAGE_KEY);
+                observer.handleFailure(message);
+            } else if (msg.getData().containsKey(GetFeedTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(GetFeedTask.EXCEPTION_KEY);
+                observer.handleException(ex);
+            }
+        }
+    }
+
+    /**
+     * Message handler (i.e., observer) for GetStoryTask.
+     */
+    private class GetStoryHandler extends Handler {
+
+        private StatusService.GetStoryObserver observer;
+
+        public GetStoryHandler(StatusService.GetStoryObserver observer) {
             this.observer = observer;
         }
 
---
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/StoryPresenter.java
@@ -0,0 +1,115 @@
+package edu.byu.cs.tweeter.client.presenter;
+
+import java.util.List;
+
+import edu.byu.cs.tweeter.client.cache.Cache;
+import edu.byu.cs.tweeter.client.model.service.StatusService;
+import edu.byu.cs.tweeter.client.model.service.UserService;
+import edu.byu.cs.tweeter.model.domain.Status;
+import edu.byu.cs.tweeter.model.domain.User;
+
+public class StoryPresenter {
+    private static final int PAGE_SIZE = 10;
+
+
+    public interface View {
+        void displayMessage(String message);
+        void setLoadingStatus(boolean value);
+
+        void addStory(List<Status> story);
+
+        void startMainActivity(User thisUser);
+    }
+
+    private StoryPresenter.View view;
+    private StatusService statusService;
+    private UserService userService;
+
+    private Status lastStatus;
+
+    private boolean hasMorePages;
+    private boolean isLoading = false;
+
+    public boolean isLoading() {
+        return isLoading;
+    }
+
+    public void setLoading(boolean loading) {
+        isLoading = loading;
+    }
+
+    public boolean hasMorePages() {
+        return hasMorePages;
+    }
+
+    public void setHasMorePages(boolean hasMorePages) {
+        this.hasMorePages = hasMorePages;
+    }
+
+    public StoryPresenter(StoryPresenter.View view) {
+        this.view = view;
+        statusService = new StatusService();
+        userService = new UserService();
+    }
+
+    public void loadMoreItems(User user) {
+        if (!isLoading) {   // This guard is important for avoiding a race condition in the scrolling code.
+            isLoading = true;
+            view.setLoadingStatus(true);
+        }
+        statusService.getStory(Cache.getInstance().getCurrUserAuthToken(), user, PAGE_SIZE, lastStatus, new StoryPresenter.GetStoryObserver());
+    }
+
+
+    public void getUser(String userAlias) {
+        userService.getUser(Cache.getInstance().getCurrUserAuthToken(), userAlias, new StoryPresenter.GetUserObserver());
+    }
+
+    public class GetStoryObserver implements StatusService.GetStoryObserver {
+
+        @Override
+        public void handleSuccess(List<Status> statuses, boolean hasMorePages) {
+            isLoading = false;
+            view.setLoadingStatus(false);
+
+            lastStatus = (statuses.size() > 0) ? statuses.get(statuses.size() - 1) : null;
+            setHasMorePages(hasMorePages);
+
+            view.addStory(statuses);
+        }
+
+        @Override
+        public void handleFailure(String message) {
+            isLoading = false;
+            view.setLoadingStatus(false);
+            view.displayMessage("Failed to get story: " + message);
+        }
+
+        @Override
+        public void handleException(Exception exception) {
+            isLoading = false;
+            view.setLoadingStatus(false);
+            view.displayMessage("Failed to get story because of exception: " + exception.getMessage());
+        }
+    }
+
+    public class GetUserObserver implements UserService.GetUserObserver {
+
+        @Override
+        public void handleSuccess(User thisUser) {
+            view.startMainActivity(thisUser);
+            view.displayMessage("Getting user's profile...");
+        }
+
+        @Override
+        public void handleFailure(String message) {
+            view.displayMessage("Failed to get user's profile: " + message);
+        }
+
+        @Override
+        public void handleException(Exception exception) {
+            view.displayMessage("Failed to get user's profile because of exception: " + exception.getMessage());
+        }
+    }
+
+}

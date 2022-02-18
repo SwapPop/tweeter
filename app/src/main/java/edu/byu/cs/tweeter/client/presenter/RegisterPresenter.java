07f089e9c500faf62ad8cf73385813ec2408ec4d
Jacob Taylor <jacob.taylor0299@gmail.com>
2022-02-18 12:56:01 -0700

    MainPresenter refactors

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/RegisterPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/RegisterPresenter.java
@@ -1,5 +1,10 @@
 package edu.byu.cs.tweeter.client.presenter;
 
+import android.graphics.Bitmap;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+
 import edu.byu.cs.tweeter.client.cache.Cache;
 import edu.byu.cs.tweeter.client.model.service.UserService;
 import edu.byu.cs.tweeter.client.model.service.observer.AuthObserver;
@@ -35,5 +40,40 @@ public class RegisterPresenter extends AuthPresenter{
     @Override
     protected String getActionString() {
         return "register";
+    }
+
+    public String convertImage(Bitmap image) {
+        // Convert image to byte array.
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+        byte[] imageBytes = bos.toByteArray();
+
+        // Intentionally, Use the java Base64 encoder so it is compatible with M4.
+        return Base64.getEncoder().encodeToString(imageBytes);
+    }
+
+    public void validateRegistration(int firstNameLength, int lastNameLength, int aliasLength, char aliasSymbol, int passwordLength, Object imageToUpload) {
+        if (firstNameLength == 0) {
+            throw new IllegalArgumentException("First Name cannot be empty.");
+        }
+        if (lastNameLength == 0) {
+            throw new IllegalArgumentException("Last Name cannot be empty.");
+        }
+        if (aliasLength== 0) {
+            throw new IllegalArgumentException("Alias cannot be empty.");
+        }
+        if (aliasSymbol != '@') {
+            throw new IllegalArgumentException("Alias must begin with @.");
+        }
+        if (aliasLength < 2) {
+            throw new IllegalArgumentException("Alias must contain 1 or more characters after the @.");
+        }
+        if (passwordLength == 0) {
+            throw new IllegalArgumentException("Password cannot be empty.");
+        }
+
+        if (imageToUpload == null) {
+            throw new IllegalArgumentException("Profile image must be uploaded.");
+        }
     }
 }
